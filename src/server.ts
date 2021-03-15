b66c083a82766cf9cf3202b2c6a2ab5308b6df8c
Rafael Dias <rafael.crd98@gmail.com>
2021-03-15 09:56:05 -0300

    heroku

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import dotenv from "dotenv";
 import { app } from "./app";
 
 dotenv.config();
-app.listen(8081, ()=> console.log("Server is running!"))
+app.listen(process.env.PORT || 8081, ()=> console.log("Server is running!"))
