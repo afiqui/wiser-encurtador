66281d80189fb66f358d190fd2ba851d57e325f1
Rafael Dias <rafael.crd98@gmail.com>
2021-03-15 09:45:05 -0300

    Heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,8 @@
 module.exports = {
     "type": "postgres",
     "url" : process.env.DATABASE_URL,    
-    "migrations":[ "./src/database/migrations/**.ts"],
-    "entities": ["./src/models/**.ts"],
+    "migrations":[ "./dist/src/database/migrations/**.ts"],
+    "entities": ["./dist/src/models/**.ts"],
     "logging" : false,
     "cli" :{
         "migrationsDir": "./src/database/migrations"
